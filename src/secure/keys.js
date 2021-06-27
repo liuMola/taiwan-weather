58cbabfda9381ace7964916a6bc5a61af52f97dc
Molajack <wer12800@gmail.com>
2021-06-28 01:52:02 +0800

    setting button and header component

---
+++ src/secure/keys.js
@@ -0,0 +1,5 @@
+const keys = {
+	cwbSecure: 'CWB-C352AC23-26CB-4CB5-9E21-BE5F8D11AAEC',
+};
+
+export default keys;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
 	mode: 'jit',
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bright: '#DFE6EE',
+				'bright-theme': '#E8E9ED',
+				dark: '#7B7D83',
+				'dark-theme': '#2B2F36',
+			},
+			boxShadow: {
+				'd-out': '  19px 19px 37px #15171a, -19px -19px 37px #414752',
+				'd-in': 'inset 19px 19px 37px #15171a, inset -19px -19px 37px #414752',
+			},
+		},
 	},
 	variants: {
 		extend: {},
