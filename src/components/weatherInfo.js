8a9069b961b71d41aee5a815a081464231956739
Molajack <wer12800@gmail.com>
2021-07-02 17:54:34 +0800

    before extract fetch function

--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//components
 import Status from './status';
 //lottie
 import Lottie from 'lottie-react';
@@ -9,6 +9,8 @@ import sunBrightData from '../lottie/sun-for-bright.json';
 import sunDarkData from '../lottie/sun-for-dark.json';
 import rainBrightData from '../lottie/rain-for-bright.json';
 import rainDarkData from '../lottie/rain-for-dark.json';
+import humdBrightData from '../lottie/humd-for-bright.json';
+import humdDarkData from '../lottie/humd-for-dark.json';
 //store
 import useStore from '../store/store';
 
@@ -24,10 +26,12 @@ export default function WeatherInfo() {
 	const windDarkOrBright = () => (mode.classList.contains('dark') ? windDarkData : windBrightData);
 	const sunDarkOrBright = () => (mode.classList.contains('dark') ? sunDarkData : sunBrightData);
 	const rainDarkOrBright = () => (mode.classList.contains('dark') ? rainDarkData : rainBrightData);
+	const humdDarkOrBright = () => (mode.classList.contains('dark') ? humdDarkData : humdBrightData);
 
 	const windSpeed = useStore((state) => state.currentWeatherData.windSpeed);
 	const uvIndex = useStore((state) => state.currentWeatherData.uvIndex);
 	const pop = useStore((state) => state.forecastData.pop);
+	const humd = useStore((state) => state.currentWeatherData.humidity);
 
 	return (
 		<div className='grid grid-cols-2 grid-rows-2 gap-6 mt-8'>
@@ -40,8 +44,8 @@ export default function WeatherInfo() {
 			<Status status={pop} category={index.pop[0]} unit={index.pop[1]}>
 				<Lottie className='w-7/12' animationData={rainDarkOrBright()} />
 			</Status>
-			<Status category={index.humd[0]} unit={index.humd[1]}>
-				<Lottie className='w-7/12' animationData={windDarkOrBright()} />
+			<Status status={humd * 100} category={index.humd[0]} unit={index.humd[1]}>
+				<Lottie className='w-7/12' animationData={humdDarkOrBright()} />
 			</Status>
 		</div>
 	);
