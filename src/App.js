fb893f6533863e05f8c35b3af302e50d6f9fc1b5
Molajack <wer12800@gmail.com>
2021-07-10 13:22:07 +0800

    rebuild trigger

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useEffect } from 'react';
 
 import MainInfo from './components/MainInfo';
-// import Header from './components/Header';
+import Header from './components/Header';
 import WeekForecast from './components/WeekForecast';
 import Footer from './components/Footer';
 //store
 import useWeatherStore from './store/store';
 import Setting from './components/Setting';
-
-// import useWeatherAPI from './hooks/useWeatherAPI';
 
 import { fetchWeatherForecast, fetchCurrentWeather, fetchWeekForecast, fetchSunriseNset } from './apis/fetchData';
 
@@ -32,7 +30,7 @@ function App() {
 		<>
 			<div className='bg-background-img h-full flex items-center justify-center text-dark font-roboto'>
 				<div className='weather-card relative min-w-[355px] white-glass pt-3 pb-5 px-6 rounded-3xl backdrop-blur'>
-					{/* <Header /> */}
+					<Header />
 					<MainInfo />
 					<WeekForecast />
 					<div className='w-full bg-bright opacity-30 h-px mt-6 mb-4'></div>
