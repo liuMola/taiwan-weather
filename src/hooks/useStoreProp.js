a6de901ece7caa37066dbdfcd22016f9f4e7fa6b
Molajack <wer12800@gmail.com>
2021-07-23 20:09:35 +0800

    adapt to PWA, icon switching

--- src/hooks/useStoreProp.js
+++
@@ -1,7 +0,0 @@
-import useWeatherStore from './store/weatherStore';
-
-// const useStoreProp = () => {
-
-//     const selector = useCallback((state) => state[prop], [prop])
-//     return useStore(selector)
-// }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -11,3 +12,5 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+serviceWorker.register();
